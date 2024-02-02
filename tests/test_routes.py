9f1519fc3e248871bcff1921ec23146bacfc42ab
tka7680 <antoine.kitten@gmail.com>
2024-02-02 15:47:47 +0000

    test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,16 +1,22 @@
-def test_print_response_params(client):
+def test_get_regions_status_code(client):
     """
-    This is just so you can see what type of detail you get in a response object.
-    Don't use this in your tests!
+    GIVEN a Flask test client
+    WHEN a request is made to /regions
+    THEN the status code should be 200
     """
     response = client.get("/regions")
-    print("Printing response.headers:")
-    print(response.headers)
-    print('\n Printing response.headers["Content-Type"]:')
-    print(response.headers['Content-Type'])
-    print("Printing response.status_code:")
-    print(response.status_code)
-    print("Printing response.data:")
-    print(response.data)
-    print("Printing response.json:")
-    print(response.json)
+    assert response.status_code == 200
+
+
+def test_get_regions_json(client):
+    """
+    GIVEN a Flask test client
+    AND the database contains data of the regions
+    WHEN a request is made to /regions
+    THEN the response should contain json
+    AND a JSON object for Tonga should be in the json
+    """
+    response = client.get("/regions")
+    assert response.headers["Content-Type"] == "application/json"
+    tonga = {'NOC': 'TGA', 'notes': None, 'region': 'Tonga'}
+    assert tonga in response.json
